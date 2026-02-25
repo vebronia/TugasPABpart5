4cba39431ce3927ae4213ba0881a307ab08af68d
vebronia <vitanialusi@gmail.com>
2026-02-25 14:48:57 +0800

    first commit

---
+++ lib/models/cart_item.dart
@@ -0,0 +1,15 @@
+// lib/models/cart_item.dart
+import 'product.dart';
+
+class CartItem {
+  final Product product;
+  int quantity;
+
+  CartItem({
+    required this.product,
+    this.quantity = 1,
+  });
+
+  // Calculated property
+  double get totalPrice => product.price * quantity;
+}
